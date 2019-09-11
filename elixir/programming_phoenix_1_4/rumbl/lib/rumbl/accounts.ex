ef26b083dec1956ce76d850d9874714ca00363d0
Julian Suggate <julian.suggate@gmail.com>
2019-09-11 13:20:06 +1200

    Programming Phoenix 1.4 Ch 1 -> 4
    Rumbl App with accounts context, Postgres persistence and Ecto schemas /
    changesets

---
+++ elixir/programming_phoenix_1_4/rumbl/lib/rumbl/accounts.ex
@@ -0,0 +1,35 @@
+defmodule Rumbl.Accounts do
+  @moduledoc """
+  The Accounts context.
+  """
+
+  alias Rumbl.Repo
+  alias Rumbl.Accounts.User
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @spec change_user(Rumbl.Accounts.User.t()) :: Ecto.Changeset.t()
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
+  def get_user(id) do
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
+  end
+
+  def get_user_by(params) do
+    Repo.get_by(User, params)
+  end
+
+  def list_users do
+    Repo.all(User)
+  end
+end
